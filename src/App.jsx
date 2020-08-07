52206ec98c5b70156203ba819a315abf39e79289
WallingDown <1532147890@qq.com>
2020-08-07 12:04:05 +0800

    动态数据插入列表

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,29 @@ import './App.css'
 import Add from './components/Add'
 import List from './components/List'
 import Footer from './components/Footer'
+import { nanoid } from 'nanoid'
 
 export default class App extends Component {
+	state = {
+		todos:[
+			{id:'001',title:'学习',completed:true},
+			{id:'002',title:'唱歌',completed:false},
+			{id:'003',title:'提问',completed:true},
+			{id:'004',title:'读书',completed:false},
+		]
+	}
+
+	addTodo = (title) =>{
+		const todo = {id:nanoid(),title:title,completed:false}
+		this.setState({todos:[todo,...this.state.todos]})
+	}
     render() {
+		const {todos} = this.state
         return (
             <div className="todo-container">
 				<div className="todo-wrap">
-					<Add/>
-					<List/>
+					<Add addTodo={this.addTodo}/>
+					<List todos={todos}/>
 					<Footer/>
 				</div>
 			</div>
--- src/components/Add/index.jsx
+++ src/components/Add/index.jsx
@@ -2,10 +2,25 @@ import React, { Component } from 'react'
 import './index.css'
 
 export default class Add extends Component {
+	handleKeyUp = (event) =>{
+		const {addTodo} = this.props
+		const {target:{value:userinput}} = event
+		if (event.keyCode === 13) {
+			if (!userinput.trim()) {
+				alert('不能为空！')
+				return
+			}
+			addTodo(userinput.trim())
+			event.target.value = ''
+		}
+	}
 	render() {
 		return (
 			<div className="todo-header">
-				<input type="text" placeholder="请输入你的任务名称，按回车键确认"/>
+				<input 
+				onKeyUp={this.handleKeyUp}
+				type="text" 
+				placeholder="请输入你的任务名称，按回车键确认"/>
 			</div>
 		)
 	}
